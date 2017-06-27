2e1981db5ab6d02d4d9ce61fe85d265b6a8dd53b
dnylng <iamdannyluong@gmail.com>
2017-06-27 19:07:58 -0400

    Added calc functionality

--- ViewController.swift
+++ ViewController.swift
@@ -14,6 +14,33 @@ class ViewController: UIViewController {
     // Reference to a button sound
     var btnSound: AVAudioPlayer!
     
+    // Reference to the output label
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    // Reference to the running number, stage before the result in the calc
+    var runningNum = ""
+    
+    // Reference to left val string
+    var leftValStr = ""
+    
+    // Reference to right val string
+    var rightValStr = ""
+    
+    // Reference to the final result
+    var result = ""
+    
+    // Different calculator operations
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    // Reference to the current op
+    var currentOperation = Operation.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,6 +54,7 @@ class ViewController: UIViewController {
         do {
             // Test the sound coming from the URL
             try btnSound = AVAudioPlayer(contentsOf: soundURL)
+            btnSound.volume = 0.1
             btnSound.prepareToPlay()
         } catch let err as NSError {
             // If failed, just print the err
@@ -42,6 +70,30 @@ class ViewController: UIViewController {
     // An action for pressed calc buttons
     @IBAction func numPressed(sender: UIButton) {
         playSound()
+        
+        // We assigned each button a tag, when btn is pressed, append to runningNum
+        runningNum += "\(sender.tag)"
+        outputLbl.text = runningNum
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(operation: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(operation: currentOperation)
     }
     
     // Function to play sounds
@@ -50,6 +102,46 @@ class ViewController: UIViewController {
             btnSound.stop()
         }
         btnSound.play()
+    }
+    
+    // Function to solve the operations
+    func processOperation(operation: Operation) {
+        // Make sure operation is not empty
+        if currentOperation != Operation.Empty {
+            
+            // A user selected an operator, but then selected another operator without first entering a number
+            if runningNum != "" {
+                // Set the right val str as the num user just pressed
+                rightValStr = runningNum
+                
+                // Reset the running num
+                runningNum = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)!*Double(rightValStr)!)"
+                } else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)!/Double(rightValStr)!)"
+                } else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)!-Double(rightValStr)!)"
+                } else if currentOperation == Operation.Add {
+                    result = "\(Double(leftValStr)!+Double(rightValStr)!)"
+                }
+                
+                // After finishing the calc, store it into the left in case of more operations
+                leftValStr = result
+                
+                // Set the text to the result
+                outputLbl.text = result
+            }
+            
+            // Set the current operation to the operation the user just performed
+            currentOperation = operation
+        } else {
+            // First time an operator is pressed
+            leftValStr = runningNum
+            runningNum = ""
+            currentOperation = operation
+        }
     }
 
 }
